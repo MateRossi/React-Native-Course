ff366cc8e8a8b34f54167ea9bebbc1d31e65f3a3
Mateus Rossi <78707153+MateRossi@users.noreply.github.com>
2023-03-19 18:33:33 -0300

    Finalização Seção 8 - Inicio Food App
    - Início da criação do Food App;
    - Criação do componente SearchBar.
    - useState para atualizar o estado de SearchBar.

---
+++ food/App.js
@@ -0,0 +1,18 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from 'react-navigation-stack';
+import SearchBar from "./src/components/SearchBar";
+import SearchScreen from "./src/screens/SearchScreen";
+
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen
+  },
+  {
+    initialRouteName: 'Search',
+    defaultNavigationOptions: {
+      title: 'Business Search'
+    }
+  }
+);
+
+export default createAppContainer(navigator);
